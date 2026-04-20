5f76a75176355a3fcc085edb9b24b31f7fcf6bb3
ShubhamCodeByte <shubhamsingh22401@gmail.com>
2026-04-21 01:09:16 +0530

    syntax basics

---
+++ 1.Fundamentals/1.Basics/2.statement.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  // printing the text on the console
+  cout << "first line \n";
+  cout << "Second line \n";
+  /* printing the no in the console . */
+  int i = 89;
+  cout << i << endl;
+  cout << 23 << endl;
+  return 0;
+}
---
+++ 1.Fundamentals/1.Basics/practice/1.syntax.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// using namespace std;
+
+int main()
+{
+  std::cout << "welcome to the understanding of the c++" << std::endl;
+
+  return 0;
+}
